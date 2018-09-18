9d0e2cc8a6afd1ed7d69b62bdfa3dc4ffb2bdb24
Trường giang <giangkoolpark@gmail.com>
2018-09-18 23:20:02 +0700

    del ssl

--- center/service.js
+++ center/service.js
@@ -1,11 +1,10 @@
 angular.module('CouponService', [])
     .factory('DataServices', function ($http, $q) {
-        var api_gateway_url = 'https://35.240.165.98:2018';
+        var api_gateway_url = 'http://35.240.165.98:2018';
         // var api_gateway_url = 'http://localhost:2018';
         var parameter;
         var url;
         var header = { header: { 'Conntent-Type': 'application/x-www-form-urlencoded' } };
-        var ssl = { strictSSL: false };
 
         return {
             signIn: function (user_id, user_img) {
@@ -14,7 +13,7 @@ angular.module('CouponService', [])
                     user_img: user_img
                 });
                 url = api_gateway_url + '/signin';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             signUp: function (user_id, user_img, info, point_per_day, point_per_today, total_slot, _class, download, access_time_per_day, point_plus, point_bad, total_list_coupon, empty_slot, use_coupon, call_server_in_day, role, _status) {
                 parameter = JSON.stringify({
@@ -37,7 +36,7 @@ angular.module('CouponService', [])
                     _status: _status
                 });
                 url = api_gateway_url + '/signup';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             Update: function (_id, fulname, email, sex, mobile, work, bithday, full_update) {
                 parameter = JSON.stringify({
@@ -51,7 +50,7 @@ angular.module('CouponService', [])
                     full_update: full_update
                 });
                 url = api_gateway_url + '/updatepro';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             Upname: function (userid, fulname) {
                 parameter = JSON.stringify({
@@ -59,7 +58,7 @@ angular.module('CouponService', [])
                     fulname: fulname
                 });
                 url = api_gateway_url + '/updatename';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             Point: function (_id, point, action_point) {
                 parameter = JSON.stringify({
@@ -68,38 +67,38 @@ angular.module('CouponService', [])
                     action_point: action_point
                 });
                 url = api_gateway_url + '/plus';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             Bad: function (_id) {
                 parameter = JSON.stringify({
                     _id: _id
                 });
                 url = api_gateway_url + '/bad';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             updateClass: function (_id) {
                 parameter = JSON.stringify({
                     _id: _id
                 });
                 url = api_gateway_url + '/updateClass';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             getBasiccode: function () {
                 url = api_gateway_url + '/getbasic';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             getAllshop: function () {
                 url = api_gateway_url + '/getshop';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             getByshopid: function (_id) {
                 parameter = JSON.stringify({ _id: _id });
                 url = api_gateway_url + '/getByshopid';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             getshopvip: function () {
                 url = api_gateway_url + '/getshopvip';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             updateCoupon: function (_id, the_issuer, new_list_coupon, user_coupon, user_id, total_slot) {
                 parameter = JSON.stringify({
@@ -111,7 +110,7 @@ angular.module('CouponService', [])
                     total_slot: total_slot
                 });
                 url = api_gateway_url + '/updatecoupon';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             updateshopLike: function (_id, user_id, user_name) {
                 parameter = JSON.stringify({
@@ -120,14 +119,14 @@ angular.module('CouponService', [])
                     user_name: user_name
                 });
                 url = api_gateway_url + '/updateshopLike';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             getActionperday: function (user_id) {
                 parameter = JSON.stringify({
                     user_id: user_id
                 });
                 url = api_gateway_url + '/get_action_for_user_per_day';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             updateActionuser: function (_id, user_id) {
                 parameter = JSON.stringify({
@@ -135,7 +134,7 @@ angular.module('CouponService', [])
                     user_id: user_id
                 });
                 url = api_gateway_url + '/updateactionuser';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             },
             createReaction: function (kind_reaction, id_post_reaction, url_post_reaction, click_reaction_day, id_shop, id_user) {
                 parameter = JSON.stringify({
@@ -147,7 +146,7 @@ angular.module('CouponService', [])
                     id_user: id_user
                 });
                 url = api_gateway_url + '/creaction';
-                return $http.post(url, parameter, header, ssl);
+                return $http.post(url, parameter, header);
             }
         }
     })
