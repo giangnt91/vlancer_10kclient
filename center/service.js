551ceb7e08878f18f5abbfaec6c4802050a78a4c
Trường giang <giangkoolpark@gmail.com>
2018-10-09 23:54:42 +0700

    add https

--- center/service.js
+++ center/service.js
@@ -1,6 +1,7 @@
 angular.module('CouponService', [])
 .factory('DataServices', function ($http, $q) {
-	var api_gateway_url = 'http://35.240.165.98:2018';
+	var api_gateway_url = 'https://api.coupon10k.com';
+	// var api_gateway_url = 'http://35.240.165.98:2018';
 	// var api_gateway_url = 'http://localhost:2018';
 	var parameter;
 	var url;
@@ -86,99 +87,99 @@ angular.module('CouponService', [])
 		},
 		Point: function (_id, point, action_point) {
 			parameter = JSON.stringify({
-	_id: _id,
-	point: point,
-	action_point: action_point
-	});
-	url = api_gateway_url + '/plus';
-	return $http.post(url, parameter, header);
-	},
-	Bad: function (_id) {
-	parameter = JSON.stringify({
-	_id: _id
-	});
-	url = api_gateway_url + '/bad';
-	return $http.post(url, parameter, header);
-	},
-	updateClass: function (_id) {
-	parameter = JSON.stringify({
-	_id: _id
-	});
-	url = api_gateway_url + '/updateClass';
-	return $http.post(url, parameter, header);
-	},
-	getBasiccode: function () {
-	url = api_gateway_url + '/getbasic';
-	return $http.post(url, parameter, header);
-	},
-	getAllshop: function () {
-	url = api_gateway_url + '/getshop';
-	return $http.post(url, parameter, header);
-	},
-	getByshopid: function (_id) {
-	parameter = JSON.stringify({ _id: _id });
-	url = api_gateway_url + '/getByshopid';
-	return $http.post(url, parameter, header);
-	},
-	getshopvip: function () {
-	url = api_gateway_url + '/getshopvip';
-	return $http.post(url, parameter, header);
-	},
-	updateCoupon: function (_id, the_issuer, new_list_coupon, user_coupon, user_id, total_slot) {
-	parameter = JSON.stringify({
-	_id: _id,
-	user_id: user_id,
-	the_issuer: the_issuer,
-	user_coupon: user_coupon,
-	new_list_coupon: new_list_coupon,
-	total_slot: total_slot
-	});
-	url = api_gateway_url + '/updatecoupon';
-	return $http.post(url, parameter, header);
-	},
-	updateshopLike: function (_id, user_id, user_name) {
-	parameter = JSON.stringify({
-	_id: _id,
-	user_id: user_id,
-	user_name: user_name
-	});
-	url = api_gateway_url + '/updateshopLike';
-	return $http.post(url, parameter, header);
-	},
-	getActionperday: function (user_id) {
-	parameter = JSON.stringify({
-	user_id: user_id
-	});
-	url = api_gateway_url + '/get_action_for_user_per_day';
-	return $http.post(url, parameter, header);
-	},
-	updateActionuser: function (_id, user_id) {
-	parameter = JSON.stringify({
-	_id: _id,
-	user_id: user_id
-	});
-	url = api_gateway_url + '/updateactionuser';
-	return $http.post(url, parameter, header);
-	},
-	createReaction: function (kind_reaction, id_post_reaction, url_post_reaction, click_reaction_day, id_shop, id_user) {
-	parameter = JSON.stringify({
-	kind_reaction: kind_reaction,
-	id_post_reaction: id_post_reaction,
-	url_post_reaction: url_post_reaction,
-	click_reaction_day: click_reaction_day,
-	id_shop: id_shop,
-	id_user: id_user
-	});
-	url = api_gateway_url + '/creaction';
-	return $http.post(url, parameter, header);
-	},
-	getEmarket: function () {
-	url = api_gateway_url + '/getemarket';
-	return $http.post(url, parameter, header);
-	},
-	GetSlider: function () {
-	url = api_gateway_url + '/getslider';
-	return $http.post(url, parameter, header);
+				_id: _id,
+				point: point,
+				action_point: action_point
+			});
+			url = api_gateway_url + '/plus';
+			return $http.post(url, parameter, header);
+		},
+		Bad: function (_id) {
+			parameter = JSON.stringify({
+				_id: _id
+			});
+			url = api_gateway_url + '/bad';
+			return $http.post(url, parameter, header);
+		},
+		updateClass: function (_id) {
+			parameter = JSON.stringify({
+				_id: _id
+			});
+			url = api_gateway_url + '/updateClass';
+			return $http.post(url, parameter, header);
+		},
+		getBasiccode: function () {
+			url = api_gateway_url + '/getbasic';
+			return $http.post(url, parameter, header);
+		},
+		getAllshop: function () {
+			url = api_gateway_url + '/getshop';
+			return $http.post(url, parameter, header);
+		},
+		getByshopid: function (_id) {
+			parameter = JSON.stringify({ _id: _id });
+			url = api_gateway_url + '/getByshopid';
+			return $http.post(url, parameter, header);
+		},
+		getshopvip: function () {
+			url = api_gateway_url + '/getshopvip';
+			return $http.post(url, parameter, header);
+		},
+		updateCoupon: function (_id, the_issuer, new_list_coupon, user_coupon, user_id, total_slot) {
+			parameter = JSON.stringify({
+				_id: _id,
+				user_id: user_id,
+				the_issuer: the_issuer,
+				user_coupon: user_coupon,
+				new_list_coupon: new_list_coupon,
+				total_slot: total_slot
+			});
+			url = api_gateway_url + '/updatecoupon';
+			return $http.post(url, parameter, header);
+		},
+		updateshopLike: function (_id, user_id, user_name) {
+			parameter = JSON.stringify({
+				_id: _id,
+				user_id: user_id,
+				user_name: user_name
+			});
+			url = api_gateway_url + '/updateshopLike';
+			return $http.post(url, parameter, header);
+		},
+		getActionperday: function (user_id) {
+			parameter = JSON.stringify({
+				user_id: user_id
+			});
+			url = api_gateway_url + '/get_action_for_user_per_day';
+			return $http.post(url, parameter, header);
+		},
+		updateActionuser: function (_id, user_id) {
+			parameter = JSON.stringify({
+				_id: _id,
+				user_id: user_id
+			});
+			url = api_gateway_url + '/updateactionuser';
+			return $http.post(url, parameter, header);
+		},
+		createReaction: function (kind_reaction, id_post_reaction, url_post_reaction, click_reaction_day, id_shop, id_user) {
+			parameter = JSON.stringify({
+				kind_reaction: kind_reaction,
+				id_post_reaction: id_post_reaction,
+				url_post_reaction: url_post_reaction,
+				click_reaction_day: click_reaction_day,
+				id_shop: id_shop,
+				id_user: id_user
+			});
+			url = api_gateway_url + '/creaction';
+			return $http.post(url, parameter, header);
+		},
+		getEmarket: function () {
+			url = api_gateway_url + '/getemarket';
+			return $http.post(url, parameter, header);
+		},
+		GetSlider: function () {
+			url = api_gateway_url + '/getslider';
+			return $http.post(url, parameter, header);
+		}
 	}
-	}
-    })	
+})	
