853f68d7b04064d538dcf4abd2f31bea098e96ea
Trường giang <giangkoolpark@gmail.com>
2018-10-24 21:33:42 +0700

    check

--- center/service.js
+++ center/service.js
@@ -186,8 +186,8 @@ angular.module('CouponService', [])
 })	
 
 .factory('Thesocket', function (socketFactory) {
-        // var api_gateway_url = 'https://api.coupon10k.com';
-		var api_gateway_url = 'http://35.240.165.98:2018';
+        var api_gateway_url = 'https://api.coupon10k.com';
+		// var api_gateway_url = 'http://35.240.165.98:2018';
         var socketConnection = io.connect(api_gateway_url);
         var socket = socketFactory({
             ioSocket: socketConnection
