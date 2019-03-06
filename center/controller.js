3b95c8a5804efa253713ebdecd9f4862a1c36030
Trường giang <giangkoolpark@gmail.com>
2019-03-06 11:05:05 +0700

    fix bug

--- center/controller.js
+++ center/controller.js
@@ -75,7 +75,7 @@ coupon
 								DataServices.signUp(fbres.authResponse.userID, Imgurl, JSON.stringify($scope.info), 0, 0, 5, JSON.stringify(_class), false, [], 0, 0, null, 5, [], null, JSON.stringify(_role), $scope.access_token, JSON.stringify(_status)).then(function (signup_res) {
 									var signup_result = signup_res.data;
 									if (signup_result.error_code === 0) {
-										DataServices.signIn(fbres.userID, Imgurl).then(function (signin_res_2) {
+										DataServices.signIn(fbres.authResponse.userID, Imgurl).then(function (signin_res_2) {
 											var signin_result_2 = signin_res_2.data;
 											if (signin_result_2.error_code === 0) {
 												localStorage.setItem('auth', JSON.stringify(signin_result_2.auth));
